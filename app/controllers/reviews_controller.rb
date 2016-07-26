4d23c73fbb5a85d7e874a31428631f870fb2599f
WilfredKesh <wilfredgish5@gmail.com>
2016-07-25 23:34:53 -0400

    added raty js

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
 
 	before_action :find_book
+    before_action :set_review, only: [:edit, :update, :destroy]
 
 	def new 
 		@review = Review.new
@@ -19,8 +20,29 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+
+	def update
+		if @review.update(review_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to book_path(@book)
+	end
+
 
 	private
+
+	def set_review
+		@review = Review.find(params[:id])
+	end
 
 	def review_params
 		params.require(:review).permit(:rating, :comment)
